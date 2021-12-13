ee3176ddf37bdd5e6faf67f6755c2ddfce2ae463
Leire <lgarmendiadi20dw@ikzubirimanteo.com>
2021-12-13 13:16:46 +0100

    Añadir proyecto al repoditorio de git junto al programa factorial

---
+++ Debugger_Leire/src/Factorial.java
@@ -0,0 +1,18 @@
+
+public class Factorial {
+
+	public void factorial(int n) {
+		int fact = 1;
+		for (int i = 1; i <= n; i++) {
+			fact *= i;
+		}
+		System.out.println("El factorial de "+n+" es "+fact);
+	}
+	
+	public static void main(String[] args) {
+		Factorial f1= new Factorial();
+		f1.factorial(6);
+		
+	}
+
+}
